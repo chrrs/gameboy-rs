7a7df36d7295585cd9cf6f18ea833121bb484a41
Chris <chirssel@gmail.com>
2021-06-30 17:11:10 +0200

    Add enough instructions in order to parse the boot rom

--- src/cpu.rs
+++ src/cpu.rs
@@ -82,10 +82,14 @@ impl Cpu {
         let opcode = self.fetch_u8(mmu);
 
         match opcode {
+            0x00 => Some(Instruction::Noop),
             0x02 => Some(Instruction::Load(
                 InstructionOperand::MemoryLocationRegister(CpuRegister::BC),
                 InstructionOperand::Register(CpuRegister::A),
             )),
+            0x04 => Some(Instruction::Increment(InstructionOperand::Register(
+                CpuRegister::B,
+            ))),
             0x05 => Some(Instruction::Decrement(InstructionOperand::Register(
                 CpuRegister::B,
             ))),
@@ -94,6 +98,9 @@ impl Cpu {
                 InstructionOperand::Immediate8(self.fetch_u8(mmu)),
             )),
             0x0c => Some(Instruction::Increment(InstructionOperand::Register(
+                CpuRegister::C,
+            ))),
+            0x0d => Some(Instruction::Decrement(InstructionOperand::Register(
                 CpuRegister::C,
             ))),
             0x0e => Some(Instruction::Load(
@@ -107,9 +114,27 @@ impl Cpu {
             0x13 => Some(Instruction::Increment(InstructionOperand::Register(
                 CpuRegister::DE,
             ))),
+            0x15 => Some(Instruction::Decrement(InstructionOperand::Register(
+                CpuRegister::D,
+            ))),
+            0x16 => Some(Instruction::Load(
+                InstructionOperand::Register(CpuRegister::D),
+                InstructionOperand::Immediate8(self.fetch_u8(mmu)),
+            )),
+            0x17 => Some(Instruction::RotateLeft(InstructionOperand::Register(
+                CpuRegister::A,
+            ))),
+            0x18 => Some(Instruction::JumpRelative(self.fetch_u8(mmu) as i8)),
             0x1a => Some(Instruction::Load(
                 InstructionOperand::Register(CpuRegister::A),
                 InstructionOperand::MemoryLocationRegister(CpuRegister::DE),
+            )),
+            0x1d => Some(Instruction::Decrement(InstructionOperand::Register(
+                CpuRegister::E,
+            ))),
+            0x1e => Some(Instruction::Load(
+                InstructionOperand::Register(CpuRegister::E),
+                InstructionOperand::Immediate8(self.fetch_u8(mmu)),
             )),
             0x20 => Some(Instruction::JumpRelativeIf(
                 CpuFlag::Zero,
@@ -127,6 +152,18 @@ impl Cpu {
             0x23 => Some(Instruction::Increment(InstructionOperand::Register(
                 CpuRegister::HL,
             ))),
+            0x24 => Some(Instruction::Increment(InstructionOperand::Register(
+                CpuRegister::H,
+            ))),
+            0x28 => Some(Instruction::JumpRelativeIf(
+                CpuFlag::Zero,
+                true,
+                self.fetch_u8(mmu) as i8,
+            )),
+            0x2e => Some(Instruction::Load(
+                InstructionOperand::Register(CpuRegister::L),
+                InstructionOperand::Immediate8(self.fetch_u8(mmu)),
+            )),
             0x31 => Some(Instruction::Load(
                 InstructionOperand::Register(CpuRegister::SP),
                 InstructionOperand::Immediate16(self.fetch_u16(mmu)),
@@ -135,25 +172,65 @@ impl Cpu {
                 CpuRegister::HL,
                 InstructionOperand::Register(CpuRegister::A),
             )),
+            0x3d => Some(Instruction::Decrement(InstructionOperand::Register(
+                CpuRegister::A,
+            ))),
             0x3e => Some(Instruction::Load(
                 InstructionOperand::Register(CpuRegister::A),
                 InstructionOperand::Immediate8(self.fetch_u8(mmu)),
+            )),
+            0x4f => Some(Instruction::Load(
+                InstructionOperand::Register(CpuRegister::C),
+                InstructionOperand::Register(CpuRegister::A),
+            )),
+            0x57 => Some(Instruction::Load(
+                InstructionOperand::Register(CpuRegister::D),
+                InstructionOperand::Register(CpuRegister::A),
+            )),
+            0x67 => Some(Instruction::Load(
+                InstructionOperand::Register(CpuRegister::H),
+                InstructionOperand::Register(CpuRegister::A),
             )),
             0x77 => Some(Instruction::Load(
                 InstructionOperand::MemoryLocationRegister(CpuRegister::HL),
                 InstructionOperand::Register(CpuRegister::A),
             )),
+            0x78 => Some(Instruction::Load(
+                InstructionOperand::Register(CpuRegister::A),
+                InstructionOperand::Register(CpuRegister::B),
+            )),
             0x7b => Some(Instruction::Load(
                 InstructionOperand::Register(CpuRegister::A),
                 InstructionOperand::Register(CpuRegister::E),
             )),
+            0x7c => Some(Instruction::Load(
+                InstructionOperand::Register(CpuRegister::A),
+                InstructionOperand::Register(CpuRegister::H),
+            )),
+            0x7d => Some(Instruction::Load(
+                InstructionOperand::Register(CpuRegister::A),
+                InstructionOperand::Register(CpuRegister::L),
+            )),
+            0x86 => Some(Instruction::Add(
+                CpuRegister::A,
+                InstructionOperand::MemoryLocationRegister(CpuRegister::HL),
+            )),
+            0x90 => Some(Instruction::Subtract(InstructionOperand::Register(
+                CpuRegister::B,
+            ))),
             0xaf => Some(Instruction::Xor(InstructionOperand::Register(
                 CpuRegister::A,
             ))),
+            0xbe => Some(Instruction::Compare(
+                InstructionOperand::MemoryLocationRegister(CpuRegister::HL),
+            )),
+            0xc1 => Some(Instruction::Pop(CpuRegister::BC)),
+            0xc5 => Some(Instruction::Push(CpuRegister::BC)),
+            0xc9 => Some(Instruction::Return),
             0xcb => self.fetch_extended_instruction(mmu),
             0xcd => Some(Instruction::Call(self.fetch_u16(mmu))),
             0xe0 => Some(Instruction::Load(
-                InstructionOperand::OffsetMemoryLocationImmediate8(0xff00, self.fetch_u8(mmu)),
+                InstructionOperand::MemoryLocationImmediate16(0xff00 + self.fetch_u8(mmu) as u16),
                 InstructionOperand::Register(CpuRegister::A),
             )),
             0xe2 => Some(Instruction::Load(
@@ -163,6 +240,10 @@ impl Cpu {
             0xea => Some(Instruction::Load(
                 InstructionOperand::MemoryLocationImmediate16(self.fetch_u16(mmu)),
                 InstructionOperand::Register(CpuRegister::A),
+            )),
+            0xf0 => Some(Instruction::Load(
+                InstructionOperand::Register(CpuRegister::A),
+                InstructionOperand::MemoryLocationImmediate16(0xff00 + self.fetch_u8(mmu) as u16),
             )),
             0xfe => Some(Instruction::Compare(InstructionOperand::Immediate8(
                 self.fetch_u8(mmu),
@@ -178,6 +259,9 @@ impl Cpu {
         let opcode = self.fetch_u8(mmu);
 
         match opcode {
+            0x11 => Some(Instruction::RotateLeft(InstructionOperand::Register(
+                CpuRegister::C,
+            ))),
             0x7c => Some(Instruction::Bit(
                 7,
                 InstructionOperand::Register(CpuRegister::H),
