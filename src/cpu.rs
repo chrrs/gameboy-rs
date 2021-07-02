f7c11db3f32fc9da3df9d42d7cbea60a1efce64c
Chris <chirssel@gmail.com>
2021-07-02 15:35:06 +0200

    Add a test execution framework

--- src/cpu.rs
+++ src/cpu.rs
@@ -23,16 +23,16 @@ impl CpuFlag {
 }
 
 pub struct Cpu {
-    a: u8,
-    b: u8,
-    c: u8,
-    d: u8,
-    e: u8,
-    h: u8,
-    l: u8,
-    f: u8,
-    sp: u16,
-    pc: u16,
+    pub a: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub h: u8,
+    pub l: u8,
+    pub f: u8,
+    pub sp: u16,
+    pub pc: u16,
 }
 
 impl Cpu {
@@ -211,6 +211,10 @@ impl Cpu {
 
     pub fn exec_next_instruction(&mut self, mmu: &mut Mmu) -> usize {
         let instruction = self.fetch_instruction(mmu).unwrap();
+        self.exec_instruction(mmu, instruction)
+    }
+
+    pub fn exec_instruction(&mut self, mmu: &mut Mmu, instruction: Instruction) -> usize {
         let cycles = instruction.cycles();
 
         match instruction {
@@ -252,7 +256,7 @@ impl Cpu {
         cycles
     }
 
-    fn fetch_instruction(&mut self, mmu: &mut Mmu) -> Option<Instruction> {
+    pub fn fetch_instruction(&mut self, mmu: &mut Mmu) -> Option<Instruction> {
         let opcode = self.fetch_u8(mmu);
 
         match opcode {
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ use std::fs::File;
 use gameboy::{bios::DMG_BIOS, cartridge::Cartridge, cpu::Cpu, mmu::Mmu};
 
 fn main() {
-    let cart = Cartridge::new(File::open("./roms/Tetris.gb").unwrap()).unwrap();
+    let cart = Cartridge::new(File::open("./roms/blargg/cpu_instrs.gb").unwrap()).unwrap();
     println!("Valid cart? {}", cart.verify());
     println!("Cart title: {}", cart.title().unwrap_or("<unknown>"));
 
     let mut mmu = Mmu::new(DMG_BIOS, cart);
     let mut cpu = Cpu::new();
 
-    while let Some(instruction) = cpu.fetch_instruction(&mut mmu) {
-        println!("{:x?}", instruction)
+    while cpu.pc <= 0x100 {
+        let instruction = cpu.fetch_instruction(&mut mmu).unwrap();
+        println!(
+            "A:{:#x} B:{:#x} C:{:#x} D:{:#x} E:{:#x} H:{:#x} L:{:#x} F:{:#x}, Executing {:x?}",
+            cpu.a, cpu.b, cpu.c, cpu.d, cpu.e, cpu.h, cpu.l, cpu.f, instruction
+        );
+        cpu.exec_instruction(&mut mmu, instruction);
     }
 }
