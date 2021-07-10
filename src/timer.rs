c82385d4d3ed842e2856aa26526d0bfc26123015
Chris <chirssel@gmail.com>
2021-07-10 14:26:06 +0200

    Rewrite timer logic

--- src/timer.rs
+++ src/timer.rs
@@ -8,9 +8,8 @@ pub struct Timer {
     pub speed: u8,
     pub enabled: bool,
 
-    sub: usize,
-    div_sub: usize,
-    step_sub: usize,
+    div_clock: usize,
+    counter_clock: usize,
 }
 
 impl Timer {
@@ -23,43 +22,35 @@ impl Timer {
             speed: 0,
             enabled: false,
 
-            sub: 0,
-            div_sub: 0,
-            step_sub: 0,
+            div_clock: 0,
+            counter_clock: 0,
         }
     }
 
     pub fn cycle(&mut self, cycles: usize) -> Interrupts {
-        self.sub += cycles;
+        self.div_clock += cycles;
+        if self.div_clock >= 64 {
+            self.div_clock -= 64;
+            self.divider = self.divider.wrapping_add(1);
+        }
 
-        if self.sub >= 4 {
-            self.sub -= 4;
+        if self.enabled {
+            let period = match self.speed {
+                0b00 => 256,
+                0b01 => 4,
+                0b10 => 16,
+                0b11 => 64,
+                _ => unreachable!(),
+            };
 
-            self.div_sub += 1;
-            if self.div_sub >= 16 {
-                self.div_sub -= 16;
-                self.divider = self.divider.wrapping_add(1);
-            }
+            self.counter_clock += cycles;
+            if self.counter_clock >= period {
+                self.counter_clock -= period;
+                self.counter = self.counter.wrapping_add(1);
 
-            if self.enabled {
-                self.step_sub += 1;
-                let div = match self.speed {
-                    0b00 => 64,
-                    0b01 => 1,
-                    0b10 => 4,
-                    0b11 => 16,
-                    _ => unreachable!(),
-                };
-
-                if self.step_sub >= div {
-                    self.step_sub -= div;
-
-                    if let Some(i) = self.counter.checked_add(1) {
-                        self.counter = i;
-                    } else {
-                        self.counter = self.modulo;
-                        return Interrupts::TIMER;
-                    }
+                if self.counter == 0 {
+                    self.counter = self.modulo;
+                    return Interrupts::TIMER;
                 }
             }
         }
